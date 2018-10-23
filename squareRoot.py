198e4f05a21fcb4307c88c7a87ef644257cf1763
Viswa N <viswanatham@gmail.com>
2018-10-23 17:52:59 -0400

    squareRoot.py added additional functionality

--- squareRoot.py
+++ squareRoot.py
@@ -1,15 +1,25 @@
 #!/usr/bin/python
-theNumber = 20
-theRoot =4.0
-percent =4.47
-diff = 0.5 
-while ( abs(diff) > 0.0001):
-	theRoot = (theRoot + (theNumber/theRoot))/2
-	#theRoot = theRoot +  0.001 
-	diff = theNumber - (theRoot * theRoot) 
-	print "theroot:diff:" + str(theRoot) +":" + str(diff)
-print "theroot:diff:" + str(theRoot) +":" + str(diff)
+#
+#	Calculate root of a number without using sqr root function.
+#	Name:squareRoot
+#
+
+#theNumber = 20
+def	sqrRoot(theNumber):
+        diff = 0.5
+	theRoot =1.0
+	while ( abs(diff) > 0.0001):
+		theRoot = (theRoot + (theNumber/theRoot))/2
+		#theRoot = theRoot +  0.001 
+		diff = theNumber - (theRoot * theRoot) 
+	#print "theroot:diff:" + str(theRoot) +":" + str(diff)
+	return theRoot
    
+print "Enter a number:",
+x= input()
+theNumber = int(x)
+print (sqrRoot(theNumber))
+
 
 
 
